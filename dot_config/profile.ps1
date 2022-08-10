6d0e05ae7c505ccaf27a24fc38bb338765283dd1
Blair Conrad <blair@blairconrad.com>
2022-08-10 14:50:44 -0400

    Add common powershell environment settings

--- dot_config/profile.ps1
+++ dot_config/profile.ps1
@@ -1,1 +1,39 @@
+$env:DEFAULT_EDITOR = "code"
 $env:PYTHONSTARTUP = Join-Path $env:XDG_CONFIG_HOME pythonrc.py
+
+Import-Module oh-my-posh
+Import-Module posh-git
+Import-Module PSReadLine
+
+Function Set-LocationToParent {
+    Set-Location ..
+}
+
+Set-Alias .. Set-LocationToParent
+
+Function Activate-VirtualEnvironment {
+    $venvPath = ".venv/Scripts/Activate.ps1"
+    if (! (Test-Path $venvPath)) {
+        if ("y" -eq (Read-Host -Prompt "There's no $venvPath in this directory Create one?")) {
+            python -m venv .venv
+        }
+        else {
+            Write-Host "Okay. Nevermind."
+            return
+        }
+    }
+    & $venvPath
+}
+
+Set-Alias venv Activate-VirtualEnvironment
+
+Set-PSReadLineOption -BellStyle Visual
+Set-PSReadLineOption -EditMode Emacs
+
+Set-PSReadlineKeyHandler -Key "Ctrl+LeftArrow" -Function BackwardWord
+Set-PSReadlineKeyHandler -Key "Ctrl+RightArrow" -Function ForwardWord
+Set-PSReadlineKeyHandler -Key "Tab" -Function Complete
+
+# oh-my-posh settings
+Set-Theme Paradox
+$DefaultUser = $env:USERNAME
