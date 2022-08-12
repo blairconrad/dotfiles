18157960ecdfcbf696a50dae67b7c7c133a58946
Blair Conrad <blair@blairconrad.com>
2022-08-11 21:38:45 -0400

    Add windows packages to install

--- run_onchange_install-packages.ps1
+++ run_onchange_install-packages.ps1
@@ -1,5 +1,14 @@
 @(
-    "JanDeDobbeleer.OhMyPosh"
+    "Git.Git",
+    "Google.Chrome",
+    "JanDeDobbeleer.OhMyPosh",
+    "Microfost.VisualStudioCode"
 ) | Foreach-Object {
     winget install --scope user --exact --accept-package-agreements --accept-source-agreements $_
+}
+
+@(
+    "pyenv"
+) | Foreach-Object {
+    scoop install $_
 }
