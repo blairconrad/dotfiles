071a8d69b718c6ee5cd4d1257cd63fd1bfe85a17
Blair Conrad <blair@blairconrad.com>
2022-10-28 11:57:22 -0400

    Install pyenv-win via powershell, not scoop
    Due to problems that lead me to create
    [scoop-installed pyenv install fails with 'error installing "core" component MSI'](https://github.com/pyenv-win/pyenv-win/issues/449)

--- run_onchange_install-packages.ps1
+++ run_onchange_install-packages.ps1
@@ -14,11 +14,17 @@
     }
 }
 
+Invoke-WebRequest `
+    -UseBasicParsing `
+    -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" `
+    -OutFile "./install-pyenv-win.ps1"; `
+    &"./install-pyenv-win.ps1"; `
+    Remove-Item "./install-pyenv-win.ps1"
+
 @(
     "autohotkey2",
     "bat",
-    "less",
-    "pyenv"
+    "less"
 ) | Foreach-Object {
     scoop install $_
 }
