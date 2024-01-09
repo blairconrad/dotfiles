9e022ea417b5fd9454cbd4577aff548c2c0e83da
Blair Conrad <blair@blairconrad.com>
2024-01-09 13:42:54 -0500

    Add a bunch of packages

--- run_onchange_install-packages.ps1
+++ run_onchange_install-packages.ps1
@@ -1,17 +1,27 @@
 $wingetPackages = @(
     "dandavison.delta",
+    "dotPDNLLC.paintdotnet",
+    "EclipseAdoptium.Temurin.11.JDK",
+    "EclipseAdoptium.Temurin.17.JDK",
+    "EclipseAdoptium.Temurin.8.JDK",
     "Git.Git",
     "Google.Chrome",
     "JanDeDobbeleer.OhMyPosh",
+    "Microsoft.PowerShell",
     "Microsoft.VisualStudioCode",
-    "Microsoft.WindowsTerminal"
+    "Microsoft.WindowsTerminal",
+    "OpenWhisperSystems.Signal",
+    "Python.Python.3.10",
+    "Python.Python.3.11",
+    "Python.Python.3.8",
+    "Python.Python.3.9",
+    "sharkdp.bat",
+    "sharkdp.fd"
 )
 winget install --no-upgrade --scope user --exact --accept-package-agreements --accept-source-agreements $wingetPackages
 
 @(
-    "7zip",
-    "bat",
-    "less"
+    "7zip"
 ) | Foreach-Object {
     scoop install $_
 }
